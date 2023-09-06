7e029a902b39011c804d4933d8c9307a08392ff2
Thiago Cavalcante <thhyagofoz@gmail.com>
2023-09-06 19:24:08 -0300

    ajustando boa parte

--- src/test/java/desfaio/Lembretes/LembretesApplicationTests.java
+++ src/test/java/desfaio/Lembretes/LembretesApplicationTests.java
@@ -5,6 +5,7 @@ import desfaio.Lembretes.DTO.PessoaDTO;
 import desfaio.Lembretes.Entidades.Pessoa;
 import desfaio.Lembretes.Repositorio.PessoaRepositorio;
 import desfaio.Lembretes.Service.PessoaService;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,7 +25,6 @@ class LembretesApplicationTests {
 
     @MockBean
     PessoaRepositorio pessoaRepositorio;
-
     @Autowired
     PessoaController pessoaController;
     @Autowired
@@ -32,35 +32,44 @@ class LembretesApplicationTests {
 
     @BeforeEach
     void injectDado() {
-        PessoaDTO pessoa = new PessoaDTO();
-        pessoa.setId(1l);
-        pessoa.setNome("Joao");
+        Pessoa pessoa = new Pessoa(1l, "Joao");
         Long id = 1l;
-        Mockito.when(pessoaRepositorio.findById(id)).thenReturn(pessoa);
+        Mockito.when(pessoaRepositorio.buscaNome("Joao")).thenReturn(pessoa);
+
+        List<Pessoa> pessoas = new ArrayList<>();
+pessoas.add(new Pessoa(1l, "Joao"));
+pessoas.add(new Pessoa(2l, "Maria"));
+pessoas.add(new Pessoa(3l, "Jose"));
+        Mockito.when(pessoaRepositorio.findAll()).thenReturn(pessoas);
     }
 
-    @BeforeEach
-    void injectLista() {
-        List<Pessoa> pessoa = new ArrayList<>();
-pessoa.add(new Pessoa(1l, "Joao"));
-pessoa.add(new Pessoa(2l, "Maria"));
-pessoa.add(new Pessoa(3l, "Jose"));
-        Mockito.when(pessoaRepositorio.findAll()).thenReturn(pessoa);
-    }
+//    @BeforeEach
+//    void injectLista() {
+//        List<Pessoa> pessoa = new ArrayList<>();
+//pessoa.add(new Pessoa(1l, "Joao"));
+//pessoa.add(new Pessoa(2l, "Maria"));
+//pessoa.add(new Pessoa(3l, "Jose"));
+//        Mockito.when(pessoaRepositorio.findAll()).thenReturn(pessoa);
+//    }
 
 
 
     @Test
     public void testeControllerPessoaFindbyName() {
-        var pessoController = pessoaController.retornaPessoa("Joao");
-        Long id = pessoController.getBody().getId().longValue();
+        var pessoa = pessoaController.retornaPessoa("Joaoss");
+        Long id = pessoa.getBody().getId().longValue();
         Assert.assertEquals(1l, id, 0);
     }
 
     @Test
     public void testeControllerPessoaFindAll() {
         var pessoController = pessoaController.retornaLista();
-        Assertions.assertEquals(3, pessoController.getBody().size());
+        Assert.assertEquals(3, pessoController.getBody().size());
+        }
+
+        @Test
+    public void testeControllerCadastro(){
+                var pessoa = pessoaController.cadastraPessoa()
         }
 
 
